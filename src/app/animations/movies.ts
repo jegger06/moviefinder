219b8f9cb04f9f3441eff5781e6c21eea2249728
ph2150092 <ph2150092@transcosmos-ph.local>
2017-10-06 16:17:13 +0800

    Replaced the loader image

--- src/app/animations/movies.ts
+++ src/app/animations/movies.ts
@@ -78,35 +78,23 @@ export const movieListTrigger = trigger('movieList', [
 
 export const movieImageTrigger = trigger('movieImage', [
     transition(':enter', [
-        animate('600ms ease-in', keyframes([
-            style({
-                opacity: 0,
-                offset: 0
-            }),
-            style({
-                opacity: 0.4,
-                offset: 0.3
-            }),
-            style({
-                opacity: 1,
-                offset: 1
-            })
-        ]))
+        style({
+            opacity: 0,
+            offset: 0
+        }),
+        animate('1000ms ease-out', style({
+            opacity: 1,
+            offset: 1
+        }))
     ]),
     transition(':leave', [
-        animate('600ms ease-out', keyframes([
-            style({
-                opacity: 1,
-                offset: 0
-            }),
-            style({
-                opacity: 0.4,
-                offset: 0.3
-            }),
-            style({
-                opacity: 0,
-                offset: 1
-            })
-        ]))
+        style({
+            opacity: 1,
+            offset: 0
+        }),
+        animate('100ms ease-out', style({
+            opacity: 0,
+            offset: 1
+        }))
     ])
 ]);
